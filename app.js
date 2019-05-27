c2b619c572e3768f153e786a614c5786a2a4e03b
thinker <229152918@qq.com>
2019-05-27 11:57:04 +0800

    Create Express Router Modules

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const app = express();
 // 设置监听端口
 app.listen(3000,()=>console.log('Server is running on 3000 port'));
 
-// 注册路由
-app.get('/',(req, res)=>{
-    res.send('Welcome to app');
-});
+// 5、导入路由模块
+const router = require('./routers.js');
+
+// 6、挂载路由到app实例
+app.use(router);
+
+
