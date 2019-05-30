41edb32e5fc08652a6d8feda9f1f512aee8a0824
thinker <229152918@qq.com>
2019-05-30 14:37:16 +0800

    Arrangement Jquery-form Within Send page

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,10 +1,20 @@
-exports.topicForm = (req, res)=>{
-    // 返回视图文件
-    res.render('./topic/create',{
-        user:req.session.user
+const topic = require('../models/topic');
+
+exports.topicForm = (req, res, next)=>{
+    topic.getAll((err, topics)=>{
+        if(err){
+            return next(err);
+        }
+        console.log(topics);
+        // 返回视图文件
+        res.render('./topic/create',{
+            topics
+        });
     });
 }
-exports.topicStore = (req, res)=>{}
+exports.topicStore = (req, res)=>{
+    res.send('success');
+}
 exports.topicShow = (req, res)=>{}
 exports.topicEdit = (req, res)=>{}
 exports.topicUpdate = (req, res)=>{}
