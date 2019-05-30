920a00e3c0f10655691bb450106524a65f6db6eb
thinker <229152918@qq.com>
2019-05-30 15:29:24 +0800

    Arrangement Moment & Accomplishment Topic Store

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,19 +1,35 @@
 const topic = require('../models/topic');
+const moment = require('moment');
 
 exports.topicForm = (req, res, next)=>{
     topic.getAll((err, topics)=>{
         if(err){
             return next(err);
         }
-        console.log(topics);
+        // console.log(topics);
         // 返回视图文件
         res.render('./topic/create',{
             topics
         });
     });
 }
-exports.topicStore = (req, res)=>{
-    res.send('success');
+exports.topicStore = (req, res, next)=>{
+    const body = req.body;
+    const content = {
+        ...body,
+        userId:req.session.user.id,
+        createdAt:moment().format('YYYY-MM-DD HH:mm:ss')
+    };
+    // console.log(content);
+    topic.store(content, (err, result)=>{
+        if(err){
+            return next(err);
+        }
+        res.status(200).json({
+            code:0,
+            message:'success'
+        });
+    });
 }
 exports.topicShow = (req, res)=>{}
 exports.topicEdit = (req, res)=>{}
--- models/topic.js
+++ models/topic.js
@@ -4,3 +4,13 @@ exports.getAll = callback =>{
     const sql = "select * from topic_category";
     query(sql, callback);
 }
+
+exports.store = (content, callback) =>{
+    const sql = "insert into topics set id=default,?";
+    query(sql, [content], (err, result)=>{
+        if(err){
+            return callback(err)
+        }
+        callback(null, result);
+    });
+}
