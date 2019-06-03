88dda7a13ac280c9db25af18250830e62d5f9b4e
thinker <229152918@qq.com>
2019-06-03 20:49:02 +0800

    Deal with difference between Synchronouse and Asynchronous

--- middleware/authen.js
+++ middleware/authen.js
@@ -1,6 +1,19 @@
 module.exports = (req, res, next)=>{
 
+    // 如果没登陆，可能是还没有登陆，
+    // 还可能是登陆状态超时，
+    // 还可能是cookie被清掉
     if(!req.session.user){
+
+        // 获取请求的类型
+        const asynReq = req.get('X-Requested-With');
+        // 如果是异步请求
+        if(asynReq && asynReq === 'XMLHttpRequest'){
+            // 则返回状态码，将前端ajax处理
+            return res.sendStatus(401);
+        }
+
+        // 如果是同步则，服务器直接返回重定向
         return res.redirect('/signin');
     }
 
